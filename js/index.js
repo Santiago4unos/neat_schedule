a59012bb849d9782024c8196d6d9ea863eb88a63
Santiago <santimunozruiz@gmail.com>
2025-08-31 20:44:09 -0600

    Creo que ya terminé por ahora

--- js/index.js
+++ js/index.js
@@ -22,7 +22,13 @@ import fileUpload from "express-fileupload";
 import pdfParse from "pdf-parse";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import * as pdfjsLib from 'pdfjs-dist';
+import { text } from "stream/consumers";
+
+const pdfjs = await import("pdfjs-dist/legacy/build/pdf.mjs");
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL('../node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs',import.meta.url,).toString();
+pdfjs.GlobalWorkerOptions.standardFontDataUrl = new URL('../node_modules/pdfjs-dist/standard_fonts',import.meta.url,).toString();
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -39,7 +45,8 @@ app.get("/", (req, res) => {
 });
 
 async function extractWithPositions(pdfBuffer) {
-    const pdf = await pdfjsLib.getDocument(pdfBuffer).promise;
+    const uint8Array = new Uint8Array(pdfBuffer);
+    const pdf = await pdfjs.getDocument({data: uint8Array, disableFontFace: false}).promise;
     const page = await pdf.getPage(1);
     const textContent = await page.getTextContent();
     
@@ -55,15 +62,16 @@ async function extractWithPositions(pdfBuffer) {
     return columns;
 }
 
-function groupByColumn(items, tolerance = 10) {
+function groupByColumn(items, tolerance = 40) {
     const columns = {};
-    
     items.forEach(item => {
         const columnKey = Math.round(item.x / tolerance) * tolerance;
-        if (!columns[columnKey]) {
-            columns[columnKey] = [];
+        if (item.text != "" && item.text != " ") {
+            if (!columns[columnKey]) {
+                columns[columnKey] = [];
+            }
+            columns[columnKey].push(item);
         }
-        columns[columnKey].push(item);
     });
     
     return columns;
@@ -75,7 +83,7 @@ app.post("/upload", async (req, res) => {
     }
     
     try {
-        const data = await pdfParse(req.files.pdf.data);
+        const data = await extractWithPositions(req.files.pdf.data);
         res.json(data);
     } catch (error) {
         res.status(500).send("Error parsing PDF");
